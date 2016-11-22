dd68ec474da178e5ed20462351ecc496cbe8fa54
cweber <cweber@EJCJ0958.EJCJ.info>
2016-11-22 15:14:05 -0200

    ProdutosBean

--- src/java/br/edu/ifrs/modelo/bean/ProdutosBean.java
+++ src/java/br/edu/ifrs/modelo/bean/ProdutosBean.java
@@ -7,7 +7,7 @@ package br.edu.ifrs.modelo.bean;
 
 /**
  *
- * @author 10070206
+ * @author 10070206 - Cristian
  */
 public class ProdutosBean {
     private int id;
@@ -22,7 +22,6 @@ public class ProdutosBean {
     private int id_categoria ;
     private String situacao;
     private String observacoes;
-    
     
     
     public int getId() {
@@ -54,4 +53,62 @@ public class ProdutosBean {
     }
     
     /* continua */
+    public float getPreco_Unitario() {
+        return preco_unitario;
+    }
+    public void setPreco_Unitario(float preco_unitario) {
+        this.preco_unitario = preco_unitario;
+    }
+    
+    public String getParcelamento_Pagamento() {
+        return parcelamento_pagamento;
+    }
+    public void setParcelamento_Pagamento(String parcelamento_pagamento) {
+        this.parcelamento_pagamento = parcelamento_pagamento;
+    }
+    
+    public String getCondicoes_Entrega() {
+        return condicoes_entrega;
+    }
+    public void setCondicoes_Entrega(String condicoes_entrega) {
+        this.condicoes_entrega = condicoes_entrega;
+    }        
+    
+    public String getFoto() {
+        return foto;
+    }
+    public void setFoto(String foto) {
+        this.nome = foto;
+    }
+    
+    public float getQuantidade_Estoque() {
+        return quantidade_estoque;
+    }
+    public void setQuantidade_Estoque(float quantidade_estoque) {
+        this.quantidade_estoque = quantidade_estoque;
+    }
+    
+    public int getId_Categoria() {
+        return id_categoria;
+    }
+    public void setId_Categoria(int id_categoria) {
+        this.id_categoria = id_categoria;
+    }
+    
+    public String getSituacao() {
+        return situacao;
+    }
+
+    public void setSituacao(String situacao) {
+        this.situacao = situacao;
+    }
+
+    public String getObservacoes() {
+        return observacoes;
+    }
+
+    public void setObservacoes(String observacoes) {
+        this.observacoes = observacoes;
+    }
+    
 }
